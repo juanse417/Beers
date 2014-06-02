9a249a4c4782725921bdac4a1b7756f3257608e0
Sebas <Sebas@Sebass-MacBook-Pro.local>
2014-06-02 17:30:23 +0200

    Initial Commit

---
+++ beers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  beers
+//
+//  Created by Sebas on 02/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
